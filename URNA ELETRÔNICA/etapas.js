227d923723ea5e8fa41c4d8382c955405ed8debf
Rodrigo Fortaleza <rodrigo-edufortaleza@hotmail.com>
2021-08-22 09:04:11 -0300

    projetos RF

---
+++ URNA ELETRÔNICA/etapas.js
@@ -0,0 +1,53 @@
+let etapas = [
+  {
+    titulo: 'VEREADOR',
+    numeros: 5,
+    candidatos: [
+      {
+        numero: '99000',
+        nome: 'Susuke',
+        partido: 'UCHIHA',
+        fotos: [
+          { url: 'sasuke.png', legenda: 'Vereador' }
+        ]
+      },
+
+      {
+        numero: '16000',
+        nome: 'Naruto',
+        partido: 'SENJU',
+        fotos: [
+          { url: 'naruto.png', legenda: 'Vereador' }
+        ]
+      },
+    ]
+  },
+
+  {
+    titulo: 'PREFEITO',
+    numeros: 2,
+    candidatos: [
+      {
+        numero: '99',
+        nome: 'Madara',
+        partido: 'UCHIHA',
+        vice: 'Obito',
+        fotos: [
+          { url: 'madara.png', legenda: 'Prefeito' },
+          { url: 'obito.png', legenda: 'Vice-Prefeito', small: true }
+        ]
+      },
+
+      {
+        numero: '16',
+        nome: 'Hashirama',
+        partido: 'SENJU',
+        vice: 'Kakashi',
+        fotos: [
+          { url: 'hashirama.png', legenda: 'Prefeito' },
+          { url: 'kakashi.png', legenda: 'Vice-Prefeito', small: true }
+        ]
+      },
+    ]
+  }
+];
